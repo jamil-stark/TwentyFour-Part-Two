5c402814764f9e38ac750d22a23cdf183054109c
Lutaaya Jamil <kunoomaking@gmail.com>
2023-04-07 16:09:39 +0300

    Added options for evaluation with parentheses

--- main.c
+++ main.c
@@ -1,87 +1,95 @@
 #include <stdio.h>
-#define MAX_LINES 100
-#define MAX_NUMS 4
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 FILE *fp;
 int count = 1;
 char c;
-int easyArray [100][4];
-int mediumArray [100][4];
-int hardArray [100][4];
-int i,j;
+int easyArray[100][4];
+int i, j;
 int lines;
 int valueToUseInFileName = 0;
 int randomNumber, num1, num2, num3, num4;
 
-
-int main(){
+int main()
+{
     srand(1);
     int size;
 
     char letterChose[2];
     printf("Welcome to the game of TwentyFour Part Two!\nUse each of the four numbers shown exactly once,\ncombining them somehow with the basic mathematical operators (+,-,*,/)\nto yield the value twenty-four.");
-    chooseText(letterChose); //Prompt User to enter letter for difficulty.
+    chooseText(letterChose);                                // Prompt User to enter letter for difficulty.
     lines = changeLevelAndReturnNumberOfLines(letterChose); // call the function which returns number of lines corresponding with the selected difficulty.
-    initArray(lines); //Set the array values.
+    initArray(lines);                                       // Set the array values.
     selectRandomNumbers();
     printNumbersToUse();
 
 
-
     return 0;
-
 }
 
-
-void printNumbersToUse(){
+void printNumbersToUse()
+{
     printf("The numbers to use are: %d, %d, %d, %d", num1, num2, num3, num4);
+    while (getchar() != '\n');
+    char input[100];
+    printf("Enter an expression: ");
+    fgets(input, 100, stdin);
+    // remove newline character from input
+    input[strcspn(input, "\n")] = '\0';
+    int result = evaluate_expression(input);
+    printf("Result: %d\n", result);
+
 }
 
-
-void chooseText(char* letterChose){
+void chooseText(char *letterChose)
+{
     printf("\nChoose your difficulty level: E for easy, M for medium, and H for hard (default is easy).\nYour choice --> ");
-    scanf("%s", letterChose);  
+    scanf("%s", letterChose);
+    
+
 }
 
 
-int changeLevelAndReturnNumberOfLines(char letterToCompare[2]){
+
+int changeLevelAndReturnNumberOfLines(char letterToCompare[2])
+{
     int linecount = 0;
-      if (strcmp(letterToCompare, "M") == 0){
-        valueToUseInFileName = 1; //changed such that we keep track of level chose.
+    if (strcmp(letterToCompare, "M") == 0)
+    {
+        valueToUseInFileName = 1; // changed such that we keep track of level chose.
         linecount = countNumberOfLines("medium (1).txt");
-        
-        
     }
-    else if (strcmp(letterToCompare, "H") == 0){
+    else if (strcmp(letterToCompare, "H") == 0)
+    {
         valueToUseInFileName = 2;
-       linecount = countNumberOfLines("hard.txt");
-       
-       
+        linecount = countNumberOfLines("hard.txt");
     }
-    else{
+    else
+    {
         valueToUseInFileName = 0;
         linecount = countNumberOfLines("easy.txt");
-        
-        
     }
-    
+
     return linecount;
 }
 
-
-
-
-int countNumberOfLines(char filename[20]){
+int countNumberOfLines(char filename[20])
+{
 
     count = 1;
     fp = fopen(filename, "r");
 
-    if (fp == NULL) {
+    if (fp == NULL)
+    {
         printf("Error opening file.\n");
         return 1;
     }
 
-    while ((c = fgetc(fp)) != EOF) {
-        if (c == '\n') {
+    while ((c = fgetc(fp)) != EOF)
+    {
+        if (c == '\n')
+        {
             count++;
         }
     }
@@ -90,41 +98,212 @@ int countNumberOfLines(char filename[20]){
     return count;
 }
 
-
-
-void initArray(int size){
-    if (valueToUseInFileName == 1){
-         fp = fopen("medium (1).txt", "r");
+void initArray(int size)
+{
+    if (valueToUseInFileName == 1)
+    {
+        fp = fopen("medium (1).txt", "r");
     }
-    else if (valueToUseInFileName == 2){
-         fp = fopen("hard.txt", "r");
+    else if (valueToUseInFileName == 2)
+    {
+        fp = fopen("hard.txt", "r");
     }
     else
     {
-         fp = fopen("easy.txt", "r");
+        fp = fopen("easy.txt", "r");
     }
-    
-   
-    if (fp == NULL) {
+
+    if (fp == NULL)
+    {
         printf("Error opening file.");
         return 1;
     }
 
-        for (i = 0; i < size; i++) {
-        for (j = 0; j < 4; j++) {
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < 4; j++)
+        {
             fscanf(fp, "%d", &easyArray[i][j]);
         }
     }
-
 }
 
-
-void selectRandomNumbers(){
-    randomNumber = rand() % lines; //generate a radndom number between 0 and size of identified file.
+void selectRandomNumbers()
+{
+    randomNumber = rand() % lines; // generate a radndom number between 0 and size of identified file.
     printf("Random number: %d \n", randomNumber);
     num1 = easyArray[randomNumber][0];
     num2 = easyArray[randomNumber][1];
     num3 = easyArray[randomNumber][2];
     num4 = easyArray[randomNumber][3];
-    
+}
+
+typedef struct
+{
+    int *values;
+    char *operators;
+    int value_top;
+    int operator_top;
+} Stack;
+
+void push_value(Stack *s, int value)
+{
+    s->values[++s->value_top] = value;
+}
+
+int pop_value(Stack *s)
+{
+    return s->values[s->value_top--];
+}
+
+void push_operator(Stack *s, char op)
+{
+    s->operators[++s->operator_top] = op;
+}
+
+char pop_operator(Stack *s)
+{
+    return s->operators[s->operator_top--];
+}
+
+int is_operator(char c)
+{
+    return c == '+' || c == '-' || c == '*' || c == '/';
+}
+
+int precedence(char op)
+{
+    switch (op)
+    {
+    case '+':
+    case '-':
+        return 1;
+    case '*':
+    case '/':
+        return 2;
+    default:
+        return 0;
+    }
+}
+
+int evaluate_expression(char *input)
+{
+    Stack *stack = malloc(sizeof(Stack));
+    stack->values = malloc(strlen(input) * sizeof(int));
+    stack->operators = malloc(strlen(input) * sizeof(char));
+    stack->value_top = -1;
+    stack->operator_top = -1;
+
+    int i = 0;
+    while (input[i] != '\0')
+    {
+        if (isdigit(input[i]))
+        {
+            int value = 0;
+            while (isdigit(input[i]))
+            {
+                value = value * 10 + (input[i] - '0');
+                i++;
+            }
+            push_value(stack, value);
+        }
+        else if (is_operator(input[i]))
+        {
+            while (stack->operator_top >= 0 &&
+                   stack->operators[stack->operator_top] != '(' &&
+                   precedence(input[i]) <= precedence(stack->operators[stack->operator_top]))
+            {
+                int b = pop_value(stack);
+                int a = pop_value(stack);
+                char op = pop_operator(stack);
+                int result;
+                switch (op)
+                {
+                case '+':
+                    result = a + b;
+                    break;
+                case '-':
+                    result = a - b;
+                    break;
+                case '*':
+                    result = a * b;
+                    break;
+                case '/':
+                    result = a / b;
+                    break;
+                }
+                push_value(stack, result);
+            }
+            push_operator(stack, input[i]);
+            i++;
+        }
+        else if (input[i] == '(')
+        {
+            push_operator(stack, '(');
+            i++;
+        }
+        else if (input[i] == ')')
+        {
+            while (stack->operator_top >= 0 && stack->operators[stack->operator_top] != '(')
+            {
+                int b = pop_value(stack);
+                int a = pop_value(stack);
+                char op = pop_operator(stack);
+                int result;
+                switch (op)
+                {
+                case '+':
+                    result = a + b;
+                    break;
+                case '-':
+                    result = a - b;
+                    break;
+                case '*':
+                    result = a * b;
+                    break;
+                case '/':
+                    result = a / b;
+                    break;
+                }
+                push_value(stack, result);
+            }
+            pop_operator(stack); // discard the left parenthesis
+            i++;
+        }
+        else
+        {
+            // skip over any whitespace or unrecognized characters
+            i++;
+        }
+    }
+
+    // evaluate any remaining operators on the stack
+    while (stack->operator_top >= 0)
+    {
+        int b = pop_value(stack);
+        int a = pop_value(stack);
+        char op = pop_operator(stack);
+        int result;
+        switch (op)
+        {
+        case '+':
+            result = a + b;
+            break;
+        case '-':
+            result = a - b;
+            break;
+        case '*':
+            result = a * b;
+            break;
+        case '/':
+            result = a / b;
+            break;
+        }
+        push_value(stack, result);
+    }
+    int final_result = pop_value(stack);
+    free(stack->values);
+    free(stack->operators);
+    free(stack);
+    return final_result;
 }
--- try.c
+++ try.c
@@ -1,120 +1,187 @@
 #include <stdio.h>
-#define MAX_LINES 100
-#define MAX_NUMS 4
-FILE *fp;
-int count = 1;
-char c;
-int easyArray [100][4];
-int mediumArray [100][4];
-int hardArray [100][4];
-int i,j;
-int lines;
-int valueToUseInFileName = 0;
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
+typedef struct
+{
+    int *values;
+    char *operators;
+    int value_top;
+    int operator_top;
+} Stack;
 
-int main(){
-    int size;
-    char letterChose[2];
-    printf("Welcome to the game of TwentyFour Part Two!\nUse each of the four numbers shown exactly once,\ncombining them somehow with the basic mathematical operators (+,-,*,/)\nto yield the value twenty-four.");
-    chooseText(letterChose);
-    lines = changeLevelAndReturnNumberOfLines(letterChose);
-    // numbersToUse();
-    // char chosen[2] = ["E"];
+void push_value(Stack *s, int value)
+{
+    s->values[++s->value_top] = value;
+}
 
+int pop_value(Stack *s)
+{
+    return s->values[s->value_top--];
+}
 
-    // printf("The number of lines in the text file is: %d", lines);
-        initArray(size);
+void push_operator(Stack *s, char op)
+{
+    s->operators[++s->operator_top] = op;
+}
 
-       for (i = 0; i < size; i++) {
-        for (j = 0; j < 4; j++) {
-            printf("%d ", easyArray[i][j]);
+char pop_operator(Stack *s)
+{
+    return s->operators[s->operator_top--];
+}
+
+int is_operator(char c)
+{
+    return c == '+' || c == '-' || c == '*' || c == '/';
+}
+
+int precedence(char op)
+{
+    switch (op)
+    {
+    case '+':
+    case '-':
+        return 1;
+    case '*':
+    case '/':
+        return 2;
+    default:
+        return 0;
+    }
+}
+
+int evaluate_expression(char *input)
+{
+    Stack *stack = malloc(sizeof(Stack));
+    stack->values = malloc(strlen(input) * sizeof(int));
+    stack->operators = malloc(strlen(input) * sizeof(char));
+    stack->value_top = -1;
+    stack->operator_top = -1;
+
+    int i = 0;
+    while (input[i] != '\0')
+    {
+        if (isdigit(input[i]))
+        {
+            int value = 0;
+            while (isdigit(input[i]))
+            {
+                value = value * 10 + (input[i] - '0');
+                i++;
+            }
+            push_value(stack, value);
         }
-        printf("\n");
+        else if (is_operator(input[i]))
+        {
+            while (stack->operator_top >= 0 &&
+                   stack->operators[stack->operator_top] != '(' &&
+                   precedence(input[i]) <= precedence(stack->operators[stack->operator_top]))
+            {
+                int b = pop_value(stack);
+                int a = pop_value(stack);
+                char op = pop_operator(stack);
+                int result;
+                switch (op)
+                {
+                case '+':
+                    result = a + b;
+                    break;
+                case '-':
+                    result = a - b;
+                    break;
+                case '*':
+                    result = a * b;
+                    break;
+                case '/':
+                    result = a / b;
+                    break;
+                }
+                push_value(stack, result);
+            }
+            push_operator(stack, input[i]);
+            i++;
+        }
+        else if (input[i] == '(')
+        {
+            push_operator(stack, '(');
+            i++;
+        }
+        else if (input[i] == ')')
+        {
+            while (stack->operator_top >= 0 && stack->operators[stack->operator_top] != '(')
+            {
+                int b = pop_value(stack);
+                int a = pop_value(stack);
+                char op = pop_operator(stack);
+                int result;
+                switch (op)
+                {
+                case '+':
+                    result = a + b;
+                    break;
+                case '-':
+                    result = a - b;
+                    break;
+                case '*':
+                    result = a * b;
+                    break;
+                case '/':
+                    result = a / b;
+                    break;
+                }
+                push_value(stack, result);
+            }
+            pop_operator(stack); // discard the left parenthesis
+            i++;
+        }
+        else
+        {
+            // skip over any whitespace or unrecognized characters
+            i++;
+        }
     }
 
+    // evaluate any remaining operators on the stack
+    while (stack->operator_top >= 0)
+    {
+        int b = pop_value(stack);
+        int a = pop_value(stack);
+        char op = pop_operator(stack);
+        int result;
+        switch (op)
+        {
+        case '+':
+            result = a + b;
+            break;
+        case '-':
+            result = a - b;
+            break;
+        case '*':
+            result = a * b;
+            break;
+        case '/':
+            result = a / b;
+            break;
+        }
+        push_value(stack, result);
+    }
+    int final_result = pop_value(stack);
+    free(stack->values);
+    free(stack->operators);
+    free(stack);
+    return final_result;
+}
+
+int main()
+{
+    char input[100];
+    printf("Enter an expression: ");
+    fgets(input, 100, stdin);
+    // remove newline character from input
+    input[strcspn(input, "\n")] = '\0';
+    int result = evaluate_expression(input);
+    printf("Result: %d\n", result);
 
     return 0;
-
-}
-
-
-void chooseText(char* letterChose){
-    printf("\nChoose your difficulty level: E for easy, M for medium, and H for hard (default is easy).\nYour choice --> ");
-    scanf("%s", letterChose);  
-}
-
-
-int changeLevelAndReturnNumberOfLines(char letterToCompare[2]){
-    int linecount = 0;
-      if (strcmp(letterToCompare, "M") == 0){
-        valueToUseInFileName = 1; //changed such that we keep track of level chose.
-        linecount = countNumberOfLines("medium (1).txt");
-        
-        
-    }
-    else if (strcmp(letterToCompare, "H") == 0){
-        valueToUseInFileName = 2;
-       linecount = countNumberOfLines("hard.txt");
-       
-       
-    }
-    else{
-        valueToUseInFileName = 0;
-        linecount = countNumberOfLines("easy.txt");
-        
-        
-    }
-    
-    return linecount;
-}
-
-
-
-
-int countNumberOfLines(char filename[20]){
-
-    count = 1;
-    fp = fopen(filename, "r");
-
-    if (fp == NULL) {
-        printf("Error opening file.\n");
-        return 1;
-    }
-
-    while ((c = fgetc(fp)) != EOF) {
-        if (c == '\n') {
-            count++;
-        }
-    }
-    fclose(fp);
-
-    return count;
-}
-
-
-
-void initArray(int size){
-    if (valueToUseInFileName == 1){
-         fp = fopen("medium (1).txt", "r");
-    }
-    else if (valueToUseInFileName == 2){
-         fp = fopen("hard.txt", "r");
-    }
-    else
-    {
-         fp = fopen("easy.txt", "r");
-    }
-    
-   
-    if (fp == NULL) {
-        printf("Error opening file.");
-        return 1;
-    }
-
-        for (i = 0; i < size; i++) {
-        for (j = 0; j < 4; j++) {
-            fscanf(fp, "%d", &easyArray[i][j]);
-        }
-    }
-
 }
